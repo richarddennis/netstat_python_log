26ea84ff494b71bd58ead5a426c990a761a16e51
Richard Dennis <richard@dragoninfosec.com>
2017-09-14 16:05:38 +0100

    Update netstat.py

--- netstat.py
+++ netstat.py
@@ -130,6 +130,22 @@ def netstat():
         for line in lines_set:
             out.write(line)
 
+         #Converts ' to "
+        lines = []
+        replacements = {"'":'"'}
+        
+        with open('netstat_data_IP_formatted.json') as infile:
+            for line in infile:
+                for src, target in replacements.iteritems():
+                    line = line.replace(src, target)
+                lines.append(line)
+        with open('netstat_data_IP_formatted.json', 'w') as outfile:
+            for line in lines:
+                outfile.write(line)
+
+
+            
+
     return result
 
 def _get_pid_of_inode(inode):
