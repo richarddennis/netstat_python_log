5c35188c95a0f643cd2ad640f3e67051b721d66f
Richard Dennis <richard@dragoninfosec.com>
2017-12-06 10:25:27 +0000

    Update netstat.py

--- netstat.py
+++ netstat.py
@@ -80,7 +80,7 @@ def netstat():
         result = dict(itertools.izip(l, nline))
         print result
 	date = time.strftime("%Y-%m-%d")
-        path_to_file = "netstat_data_bridge_Vultr_three_"+date+".json"
+        path_to_file = "netstat_data_bitcoin_node_"+date+".json"
         with open(path_to_file,"a+") as f:
             f.write(str(result))
             f.write(str("\n"))
