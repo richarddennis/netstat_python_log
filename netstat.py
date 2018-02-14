49115bb9b94c4e406fe44fcb3fe58fa645733a73
Richard Dennis <richard@dragoninfosec.com>
2018-02-14 17:20:44 +0000

    Update netstat.py

--- netstat.py
+++ netstat.py
@@ -80,7 +80,7 @@ def netstat():
         result = dict(itertools.izip(l, nline))
         print result
 	date = time.strftime("%Y-%m-%d")
-        path_to_file = "netstat_data_guard_BHost_three_"+date+".json"
+        path_to_file = "netstat_data_exit_ZetServers_three_"+date+".json"
         with open(path_to_file,"a+") as f:
             f.write(str(result))
             f.write(str("\n"))
